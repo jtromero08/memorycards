2589560c4cc6cc048969b78b09a82137ff8d5374
Josue Toledo Romero <jtromero08@gmail.com>
2024-10-09 19:43:39 -0400

    Here in the CardBoard: - We added cardGenerator() - We added the CardLayout via using cards.map() - We also added the score displayer but with a add button, not the real logic yet.

--- src/pages/CardBoard/CardBoard.tsx
+++ src/pages/CardBoard/CardBoard.tsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import CardLayout from "../CardLayout/CardLayout";
+import { cardGenerator } from "./CardBoardHelper";
+
+let cards = cardGenerator();
+console.log(cards)
 
 export default function CardBoard() {
     const [score, setScore] = useState(0)
@@ -8,7 +13,14 @@ export default function CardBoard() {
                 Score: <span id="result">{score}</span>
             </h1>
             <div>
-                <button onClick={() => setScore(score+1)}></button>
+                {
+                    cards.map((card: any, index: any) => (
+                        <CardLayout key={index} card={card}/>
+                    ))
+                }
+            </div>
+            <div>
+                <button onClick={() => setScore(score+1)}>Push Me</button>
             </div>
         </div>
     )
