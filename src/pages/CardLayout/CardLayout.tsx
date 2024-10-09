aef0ba01e5f335846f7a46b46b80156ae0ae2b57
Josue Toledo Romero <jtromero08@gmail.com>
2024-10-09 19:47:00 -0400

    What we did here? - Here we fix the cardlayout logic - Now when clicking it should flipped - Card image should appear - We set a state (need improvement)

--- src/pages/CardLayout/CardLayout.tsx
+++ src/pages/CardLayout/CardLayout.tsx
@@ -1,21 +1,40 @@
-export default function CardLayout({flipCard, cardProps}: any) {
-    let card = cardProps.state;
-    let emptyAndFront = cardProps.eaf;
+import { useState } from "react";
+
+export default function CardLayout({card}: any) {
+    let cardImg;
+    const [theCard, setTheCard] = useState({
+        id: card.id,
+        cardName: card.name,
+        cardImgUrl: card.img,
+        flipped: false
+    })
 
     if(!card) return (<h1 style={{color: 'red'}}> Error: No card! </h1>);
 
+    if(theCard.flipped) {
+        cardImg = <img 
+            src={card.img} 
+            className="card__face card__face--front" 
+        />
+    } else {
+        cardImg = <img 
+            src={card.img} 
+            className="card__face card__face--back" 
+        />
+    }
+
     return (
         <div
-            onClick={() => flipCard(card.id)}
+            onClick={() => setTheCard(
+                {
+                    id: card.id,
+                    cardName: card.name,
+                    cardImgUrl: card.img,
+                    flipped: true
+                }
+            )}
         >
-            <img 
-                src={emptyAndFront[0].front} 
-                className="card__face card__face--front" 
-            />
-            <img 
-                src={card.card.img} 
-                className="card__face card__face--back" 
-            />
+            {cardImg}
         </div>
     )
 }
