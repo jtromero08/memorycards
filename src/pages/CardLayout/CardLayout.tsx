8e9810debbc20998739e34502ea4454ea9aee2d5
Josue Toledo Romero <jtromero08@gmail.com>
2024-10-16 18:45:59 -0400

    Fixed the image on how it will look and the flip design

--- src/pages/CardLayout/CardLayout.tsx
+++ src/pages/CardLayout/CardLayout.tsx
@@ -1,17 +1,17 @@
 import { CardProps } from "../../types/cardsInterfaces";
+import './CardLayout.css'; // Import the CSS file
 
 const Card: React.FC<CardProps> = ({ card, flipCard }) => {
   return (
-    <div
-      className="card"                    // Add a class for styling
-      onClick={() => flipCard(card)}     // Call flipCard with the current card on click
-      style={{ cursor: "pointer", width: "100px", height: "150px", border: "1px solid #ccc", borderRadius: "8px", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: card.isFlipped || card.isMatched ? "white" : "#eee" }}
-    >
-      {card.isFlipped || card.isMatched ? (
-        <img src={card.src} alt={card.name} style={{ width: "100%", height: "100%", borderRadius: "8px" }} />
-      ) : (
-        "?"                               // Show placeholder when not flipped
-      )}
+    <div className="card" onClick={() => flipCard(card)}>
+      <div className={`card-inner ${card.isFlipped || card.isMatched ? 'flipped' : ''}`}>
+        <div className="card-front">
+          ?
+        </div>
+        <div className="card-back">
+          <img src={card.src} alt={card.name} style={{ width: "100%", height: "100%", borderRadius: "8px" }} />
+        </div>
+      </div>
     </div>
   );
 };
