969b6471f80ab502540c1586dd61a951fc7af838
Josue Toledo Romero <jtromero08@gmail.com>
2024-10-16 18:47:11 -0400

    Fixed the types

--- src/types/cardsInterfaces.ts
+++ src/types/cardsInterfaces.ts
@@ -1,17 +1,17 @@
 export interface Card {
-    id?: number;
-    name?: string;
+    id: number;
     src: string;
-    isFlipped?: boolean;
-    isMatched?: boolean;
-}
-
-export interface CardProps {
-    card: Card;
-    flipCard: (card: Card) => void;
+    isFlipped: boolean;
+    isMatched: boolean;
+    name?: string; // Optional name property
 }
 
 export interface CardBoardProps {
     cards: Card[];
+    flipCard: (card: Card) => void;
+}
+
+export interface CardProps {
+    card: Card;
     flipCard: (card: Card) => void;
 }
